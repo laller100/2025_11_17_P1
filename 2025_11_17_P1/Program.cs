08ccacdc22048f741f1f04dca8973ab8dd28eeec
laller100 <lallerpro100@gmail.com>
2025-11-17 12:53:31 +0100

    Add project files.

---
+++ 2025_11_17_P1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2025_11_17_P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
